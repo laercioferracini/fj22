301ccac7143e41dd5439ab41a5fbc513e6928a88
laercioferracini <laercio_ferracini@yahoo.com.br>
2020-05-15 17:46:10 -0300

    Creating Custom Aggregator

--- src/test/java/br/com/ferracini/argentum/NegociacaoAggregator.java
+++ src/test/java/br/com/ferracini/argentum/NegociacaoAggregator.java
@@ -1,9 +1,26 @@
 package br.com.ferracini.argentum;
 
+import br.com.ferracini.argentum.modelo.Negociacao;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 /**
  * @author lferracini
  * @project = fj22
  * @since <pre>15/05/2020</pre>
  */
-public class NegociacaoAggregator {
+public class NegociacaoAggregator implements ArgumentsAggregator {
+    @Override
+    public Negociacao aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+        System.out.println(context.getParameter().getName());
+        return new Negociacao(
+                accessor.get(0, BigDecimal.class),
+                accessor.get(1, Integer.class),
+                accessor.get(2, LocalDateTime.class));
+    }
 }
--- src/test/java/br/com/ferracini/argentum/NegociacaoCSV.java
+++ src/test/java/br/com/ferracini/argentum/NegociacaoCSV.java
@@ -1,9 +1,19 @@
 package br.com.ferracini.argentum;
 
+import org.junit.jupiter.params.aggregator.AggregateWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author lferracini
  * @project = fj22
  * @since <pre>15/05/2020</pre>
  */
-public class CsvToPerson {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AggregateWith(NegociacaoAggregator.class)
+public @interface NegociacaoCSV {
 }
