faa0867e6a1d21689124778a3d2f18ab27a1cb0c
laercioferracini <laercio_ferracini@yahoo.com.br>
2020-05-31 17:06:34 -0300

    Fixing MediaMovelSimplesTest
    Adding more values in the list

--- fj22-backend/src/test/java/br/com/ferracini/argentum/indicadores/MediaMovelSimplesTest.java
+++ fj22-backend/src/test/java/br/com/ferracini/argentum/indicadores/MediaMovelSimplesTest.java
@@ -19,21 +19,21 @@ class MediaMovelSimplesTest {
     @Test
     void sequenciaSimplesDeCandles() {
 
-        SerieTemporal serie = GeradorDeSerie.criaSerie(1, 2, 3, 4, 3, 5, 4, 3);
+        SerieTemporal serie = GeradorDeSerie.criaSerie(1, 2, 3, 4, 3, 4, 5, 4, 3);
         MediaMovelSimples mms = new MediaMovelSimples();
 
         BigDecimal aa = BigDecimal.valueOf(10.0).divide(BigDecimal.valueOf(3), RoundingMode.HALF_DOWN);
-        BigDecimal ab = BigDecimal.valueOf(12.0).divide(BigDecimal.valueOf(3), RoundingMode.HALF_DOWN);
-
+        BigDecimal ab = BigDecimal.valueOf(13.0).divide(BigDecimal.valueOf(3), RoundingMode.HALF_DOWN);
+        BigDecimal ac = BigDecimal.valueOf(11.0).divide(BigDecimal.valueOf(3), RoundingMode.HALF_DOWN);
 
         assertAll("Verificando média simples",
                 () -> assertEquals(BigDecimal.valueOf(2.0), mms.calcula(2, serie)),
                 () -> assertEquals(BigDecimal.valueOf(3.0), mms.calcula(3, serie)),
                 () -> assertEquals(aa, mms.calcula(4, serie)),
-                () -> assertEquals(BigDecimal.valueOf(12.0 / 3), mms.calcula(5, serie)),
+                () -> assertEquals(ac, mms.calcula(5, serie)),
                 () -> assertEquals(BigDecimal.valueOf(4.0), mms.calcula(6, serie)),
-                () -> assertEquals(ab, mms.calcula(7, serie))
-                //() -> assertEquals(BigDecimal.valueOf(4.0), mms.calcula(8, serie))
+                () -> assertEquals(ab, mms.calcula(7, serie)),
+                () -> assertEquals(BigDecimal.valueOf(4.0), mms.calcula(8, serie))
         );
     }
 }
