28a36d2f157236d626378aab4d55ce79c019ff79
laercioferracini <laercio_ferracini@yahoo.com.br>
2020-06-06 21:55:59 -0300

    Creating unit test in the class MediaMovelSimplesTest to check a new Indicador parameter

--- fj22-backend/src/test/java/br/com/ferracini/argentum/indicadores/MediaMovelSimplesTest.java
+++ fj22-backend/src/test/java/br/com/ferracini/argentum/indicadores/MediaMovelSimplesTest.java
@@ -1,6 +1,7 @@
 package br.com.ferracini.argentum.indicadores;
 
 import br.com.ferracini.argentum.modelo.SerieTemporal;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -14,10 +15,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @project = fj22
  * @since <pre>29/05/2020</pre>
  */
+@DisplayName(value = "Media movel simples deve")
 class MediaMovelSimplesTest {
 
     @Test
-    void sequenciaSimplesDeCandles() {
+    @DisplayName(value ="Calcular sequencia simples de candles")
+    void calcularSequenciaSimplesDeCandles() {
         SerieTemporal serie = GeradorDeSerie.criaSerie(1, 2, 3, 4, 3, 4, 5, 4, 3);
         MediaMovelSimples mms = new MediaMovelSimples(new IndicadorFechamento());
 
@@ -37,10 +40,33 @@ class MediaMovelSimplesTest {
     }
 
     @Test
-    void sequenciaSimplesDeCandlesComIntervaloParametrizavel() {
+    @DisplayName(value = "Calcular sequência simples de candles com intervalo parametrizável")
+    void calcularSequenciaSimplesDeCandlesComIntervaloParametrizavel() {
 
         SerieTemporal serie = GeradorDeSerie.criaSerie(1, 2, 3, 4, 3, 4, 5, 4, 3);
         MediaMovelSimples mms = new MediaMovelSimples(new IndicadorFechamento());
+        int intervalo = 5;
+        BigDecimal aa = BigDecimal.valueOf(16.0).divide(BigDecimal.valueOf(intervalo), RoundingMode.HALF_DOWN);
+        BigDecimal ab = BigDecimal.valueOf(19.0).divide(BigDecimal.valueOf(intervalo), RoundingMode.HALF_DOWN);
+        BigDecimal ac = BigDecimal.valueOf(19.0).divide(BigDecimal.valueOf(intervalo), RoundingMode.HALF_DOWN);
+
+
+        assertAll("Verificando média simples parametrizável",
+
+                () -> assertEquals(aa, mms.calcula(5, serie, intervalo)),
+
+                () -> assertEquals(ab, mms.calcula(6, serie, intervalo)),
+
+                () -> assertEquals(ac, mms.calcula(8, serie, intervalo))
+        );
+    }
+
+    @Test
+    @DisplayName(value = "Calcular media movel simples com indicador de abertura")
+    void calcularMediaMovelSimplesComIndicadorAbertura() {
+
+        SerieTemporal serie = GeradorDeSerie.criaSerie(1, 2, 3, 4, 3, 4, 5, 4, 3);
+        MediaMovelSimples mms = new MediaMovelSimples(new IndicadorAbertura());
         int intervalo = 5;
         BigDecimal aa = BigDecimal.valueOf(16.0).divide(BigDecimal.valueOf(intervalo), RoundingMode.HALF_DOWN);
         BigDecimal ab = BigDecimal.valueOf(19.0).divide(BigDecimal.valueOf(intervalo), RoundingMode.HALF_DOWN);
